4f9232ecf40ce3ba943df2d3eaccaf3064a30725
Pushkraj Vyavahare <163556662+alwaysgodly@users.noreply.github.com>
2026-04-21 20:07:06 +0530

    Time: 1 ms (99.84%), Space: 55.7 MB (50.27%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ans = 0;
+        int count = 0;
+
+        for(int num: nums){
+            if(count == 0){
+                ans = num;
+            }
+
+            if(num == ans){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return ans;
+    }
+}
