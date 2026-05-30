2dfc278851b8a5b539d1e7da318aa16b0320d939
Pushkraj Vyavahare <163556662+alwaysgodly@users.noreply.github.com>
2026-05-30 11:21:13 +0530

    Time: 0 ms (100%), Space: 42.9 MB (52.15%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -16,7 +16,8 @@ class Solution {
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
-        }      
+        }
+
         return slow;
     }
 }
