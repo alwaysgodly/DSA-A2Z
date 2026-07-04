a9245fc0c908fa6848c79815c0d3683a36e214c2
Pushkraj Vyavahare <163556662+alwaysgodly@users.noreply.github.com>
2026-07-04 17:45:08 +0530

    Time: 0 ms (100%), Space: 43.9 MB (15.59%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -14,12 +14,14 @@ class Solution {
             int left = 0; int right = n-1;
 
             while(left<right){
-            int temp = matrix[i][left];
-            matrix[i][left] = matrix[i][right];
-            matrix[i][right] = temp;
-            left++;
-            right--;
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
             }
         }
+
+
     }
 }
