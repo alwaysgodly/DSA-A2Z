0cc261995b6bbb088cb993c3acea475c1accb479
Pushkraj Vyavahare <163556662+alwaysgodly@users.noreply.github.com>
2026-07-17 23:14:40 +0530

    Time: 5 ms (80.71%), Space: 45.7 MB (94.57%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,12 +1,20 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
-        for(int i = 0; i<s.length(); i++){
-            if(s.charAt(i) != s.charAt(s.length()-1-i)){
-                return false;
-            }
-            
+
+        // Negative numbers are not palindrome
+        if (x < 0) {
+            return false;
         }
-        return true;
+
+        int temp = x;
+        int reverse = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+
+        return temp == reverse;
     }
 }
