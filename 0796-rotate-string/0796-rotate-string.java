38ae1446bf3c80cd758ce3fb83256ed7038310e3
Pushkraj Vyavahare <163556662+alwaysgodly@users.noreply.github.com>
2026-09-08 22:41:15 +0530

    Time: 2 ms (64%), Space: 43.5 MB (31.76%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+        
         if(s.length() != goal.length()) return false;
 
-        String doubled = s+s;
+        String ans = s+s;
 
-        if(doubled.contains(goal)){
+        if(ans.contains(goal)){
             return true;
         }
 
